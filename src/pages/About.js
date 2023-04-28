d8432eeeb6bf05334ad6e8f2094201d9cef65956
TH876 <tayhiggins14@gmail.com>
2023-04-28 13:51:19 -0400

    Bucket list APi call implemented

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,63 @@
-function About () {
-    return ( 
-        <div>
-           <h1>THIS IS THE ABOUT PAGE</h1>
+import { useState, useEffect } from 'react';
+import SearchStyle from '../components/SearchStyle.css';
+
+
+export default function About () {
+//API ninja key: MbLwE3pabae3/a63tK6rmA==3c9pVk8icJmzz1Qf
+   //anything the user types gets set inside query
+   const [idea, setIdea] = useState('');
+
+
+   //URL "https://ski-resorts-and-conditions.p.rapidapi.com/v1/resort"
+
+   
+   // fetch("https://api.api-ninjas.com/v1/bucketlist", {
+      // method: 'GET',
+      // url: 'https://api.api-ninjas.com/v1/bucketlist',
+      // // headers: { 'X-Api-Key': 'MbLwE3pabae3/a63tK6rmA==3c9pVk8icJmzz1Qf'},
+      // contentType: 'application/json',
+      // success: function(result) {
+         //  console.log(result);
+      // },
+      // error: function ajaxError(jqXHR) {
+         //  console.error('Error: ', jqXHR.responseText);
+      // }
+//   });
+useEffect(() => {
+
+    }, []);
+
+   
+    let getBucketList = async (e) => {
+      e.preventDefault();
+      console.log("anything");
+      const res = await fetch('https://api.api-ninjas.com/v1/bucketlist', 
+      {
+      method: 'GET', 
+      
+      headers: { 
+         'Content-Type': 'application/json',
+
+         'X-Api-Key': 'MbLwE3pabae3/a63tK6rmA==3c9pVk8icJmzz1Qf'},
+   } )
+   const data = await res.json();
+   console.log(data.item);
+   setIdea(data.item);
+    }
+
+
+   return ( 
+        <div className='search'>
+           <h1>Adventure awaits</h1>
+
+            <form>
+               <button type='submit' onClick={getBucketList}>Explore Now</button>
+               {/* Display on page */}
+               <p>{idea}</p>
+            </form>
+           
         </div>
      );
 }
 
-export default About;
+// Fetch API documentation: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch 
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,17 +1,77 @@
+import { useState } from 'react';
 import SearchStyle from '../components/SearchStyle.css';
-export default function SearchPage () {
- 
-   return ( 
-        <div className='search'>
-           <h1>THIS IS THE SEARCH PAGE</h1>
-           <div className="search-container">
-            
+import PlacesAutocomplete from 'react-places-autocomplete'
+import { geocodeByAddress, geocodeByPlaceId, getLatLng, } from 'react-places-autocomplete';
 
-           </div>
-           
-           
-           
-           
-        </div>
-     );
+
+export default function SearchPage () {
+
+   const [place, setPlace] = useState();
+   const [coordinates, setCoordinates] = useState({
+      lat: null,
+      lng: null,
+   })
+
+   const handleChoice = async value => {
+      // pass in the address value and return the results 
+      const res = await geocodeByAddress(value);
+      //store the results in the getLatLng variable to get the coordinates of the address
+      const latLong = await getLatLng(res[0])
+      console.log(latLong);
+      // whatever user inputs, captures the value
+      setPlace(value);
+      setCoordinates(latLong);
+   }
+   return(
+      <div className='search-container'>
+         <h1>This is where it starts...</h1>
+         <p>Latitude: {coordinates.lat}</p>
+         <p>Longitude: {coordinates.lng}</p>
+         <p>Address: {place} </p>
+         
+      
+      <PlacesAutocomplete
+        value={place}
+        onChange={setPlace}
+        onSelect={handleChoice}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+         //the suggestions.descriptions change so need a key here
+          <div key={suggestions.descriptions}>
+            <input
+              {...getInputProps({
+                placeholder: 'Enter a Place ...',
+                className: 'location-search-input',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+
+              {/* Map through data */}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+               // The item that the cursor is placed over will have a different color from the rest of items.
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: 'blue', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+      </div>
+   )
 }
