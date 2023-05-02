c00d234ed3d43f8eaaa73aa9b74e3029ffe1f216
TH876 <tayhiggins14@gmail.com>
2023-05-02 19:00:35 -0400

    editing photo grid

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,37 @@
 import AboutStyle from '../stylecomponents/AboutStyle.css';
+import group1 from '../images/group1.jpg';
+import group2 from '../images/group2.jpg';
+import group3 from '../images/group3.jpg';
+import group4 from '../images/group4.jpg';
+import group5 from '../images/group5.jpg';
 
 function About () {
    return ( 
        <div className="about-container">
-          <h1>JustGo Travel</h1>
-          <strong>Fun. Talented. Passionate.</strong>
-          <div className='about-grid'>
-            <div className='about-left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et orci consequat, fermentum purus ut, efficitur dolor. Duis facilisis mollis mauris sed commodo. Donec consequat pulvinar magna, id suscipit arcu tincidunt eu. Nullam condimentum libero enim, ut imperdiet nisl tincidunt vitae. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec sit amet elit porta, accumsan massa blandit, ultricies mauris. Vivamus risus dolor, porttitor a enim at, auctor mollis ante. Sed eu dictum erat. Fusce aliquet eget quam vitae ornare. Etiam orci enim, hendrerit eget varius id, commodo pulvinar leo. Sed eu pulvinar nulla. Etiam rutrum molestie lectus, id elementum est rutrum tempus. Pellentesque ac tristique odio. Phasellus eget elit id nulla cursus ultrices.</div>
+         <div className='about-top'>
+         <h1>JustGo Travel</h1>
+         <strong>Fun. Talented. Passionate.</strong>
+         <div className='about-text-grid'>
+            <div className='about-left'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisi cras fermentum odio eu feugiat pretium. Nulla aliquet porttitor lacus luctus accumsan. Dictum at tempor commodo ullamcorper a lacus vestibulum. Aliquam purus sit amet luctus venenatis. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Adipiscing diam donec adipiscing tristique risus nec feugiat. Dictum sit amet justo donec. Aliquet risus feugiat in ante metus dictum at. 
+            </div>
+            
+            <div className='about-right'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisi cras fermentum odio eu feugiat pretium. Nulla aliquet porttitor lacus luctus accumsan. Dictum at tempor commodo ullamcorper a lacus vestibulum. Aliquam purus sit amet luctus venenatis. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Adipiscing diam donec adipiscing tristique risus nec feugiat. Dictum sit amet justo donec. Aliquet risus feugiat in ante metus dictum at.  
+            </div>
 
-            <div className='about-right'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et orci consequat, fermentum purus ut, efficitur dolor. Duis facilisis mollis mauris sed commodo. Donec consequat pulvinar magna, id suscipit arcu tincidunt eu. Nullam condimentum libero enim, ut imperdiet nisl tincidunt vitae. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec sit amet elit porta, accumsan massa blandit, ultricies mauris. Vivamus risus dolor, porttitor a enim at, auctor mollis ante. Sed eu dictum erat. Fusce aliquet eget quam vitae ornare. Etiam orci enim, hendrerit eget varius id, commodo pulvinar leo. Sed eu pulvinar nulla. Etiam rutrum molestie lectus, id elementum est rutrum tempus. Pellentesque ac tristique odio. Phasellus eget elit id nulla cursus ultrices.</div>
+            <div className='photo-grid'>
+               <div className='yellow'><img src={group1} alt='coworkers looking at a laptop screen around a desk'/></div>
+               <div className='pink'><img src={group2} alt='coworkers looking at a written document'/></div>
+               <div className='white'><img src={group4} alt='coworkers looking at a laptop screen'/></div>
+               <div className='purple'><img src={group3} alt='coworkers looking at a written around a desk'/></div>
+               <div className='green'><img src={group5} alt='coworkers huddling their hands together'/></div>
+            </div>
 
+         </div>
+
+            <div className='about-bottom'>
+               <h2>Our Partners</h2>
+               {/* Insert logos here! */}
+            </div>
           </div>
        </div>
     );
