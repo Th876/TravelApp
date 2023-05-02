ed997624d4e21931e3ea394d497ccd5e04cc5ada
TH876 <tayhiggins14@gmail.com>
2023-05-02 19:33:54 -0400

    Updated Our Partners section

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,6 +4,7 @@ import group2 from '../images/group2.jpg';
 import group3 from '../images/group3.jpg';
 import group4 from '../images/group4.jpg';
 import group5 from '../images/group5.jpg';
+import partnerslogos from '../images/partnerslogos.svg'; 
 
 function About () {
    return ( 
@@ -19,17 +20,19 @@ function About () {
             </div>
 
             <div className='photo-grid'>
-               <div className='yellow'><img src={group1} alt='coworkers looking at a laptop screen around a desk'/></div>
-               <div className='pink'><img src={group2} alt='coworkers looking at a written document'/></div>
-               <div className='white'><img src={group4} alt='coworkers looking at a laptop screen'/></div>
-               <div className='purple'><img src={group3} alt='coworkers looking at a written around a desk'/></div>
-               <div className='green'><img src={group5} alt='coworkers huddling their hands together'/></div>
+               <div className='group1'><img src={group1} alt='coworkers looking at a laptop screen around a desk'/></div>
+               <div className='group2'><img src={group2} alt='coworkers looking at a written document'/></div>
+               <div className='group4'><img src={group4} alt='coworkers looking at a laptop screen'/></div>
+               <div className='group3'><img src={group3} alt='coworkers looking at a written around a desk'/></div>
+               <div className='group5'><img src={group5} alt='coworkers huddling their hands together'/></div>
             </div>
 
          </div>
 
             <div className='about-bottom'>
                <h2>Our Partners</h2>
+               <img src={partnerslogos} alt="JustGo Travel partners' logo" className='partners-logo'/>
+
                {/* Insert logos here! */}
             </div>
           </div>
