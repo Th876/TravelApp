291a2c62773bedc89c22bd0d456bae958d60458a
TH876 <tayhiggins14@gmail.com>
2023-05-02 17:08:23 -0400

    editing about section

--- src/components/ReviewSlider.js
+++ src/components/ReviewSlider.js
@@ -4,7 +4,6 @@ import Carousel from 'react-elastic-carousel';
 import jennymaxwell from '../images/jennymaxwell.jpg';
 import johnallen from '../images/johnallen.jpg';
 import emmyjames from '../images/emmyjames.jpg';
-// import paulwilliams from '../images/paulwilliams.jpg';
 
 export default function ReviewSlider() {
   const [reviews, setReviews] = useState([
@@ -28,12 +27,6 @@ export default function ReviewSlider() {
         bucketlist: "See Killer Whales in the Wild",
         comment: "Really wanted to try something different after sitting at a desk job for 5 years. This is the excitement I was looking for!",
     },
-    // {
-        // image: paulwilliams,
-        // name: "Paul Williams",
-        // // bucketlist: "Raft along the Li River in Yangshuo.",
-        // // // // comment: "My year of 'yes' led me to trying JustGo travel and I’ve never made a better decision!",
-    // }
   ]);
 
   return (
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,16 @@
+import AboutStyle from '../stylecomponents/AboutStyle.css';
+
 function About () {
    return ( 
-       <div>
+       <div className="about-container">
           <h1>JustGo Travel</h1>
           <strong>Fun. Talented. Passionate.</strong>
+          <div className='about-grid'>
+            <div className='about-left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et orci consequat, fermentum purus ut, efficitur dolor. Duis facilisis mollis mauris sed commodo. Donec consequat pulvinar magna, id suscipit arcu tincidunt eu. Nullam condimentum libero enim, ut imperdiet nisl tincidunt vitae. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec sit amet elit porta, accumsan massa blandit, ultricies mauris. Vivamus risus dolor, porttitor a enim at, auctor mollis ante. Sed eu dictum erat. Fusce aliquet eget quam vitae ornare. Etiam orci enim, hendrerit eget varius id, commodo pulvinar leo. Sed eu pulvinar nulla. Etiam rutrum molestie lectus, id elementum est rutrum tempus. Pellentesque ac tristique odio. Phasellus eget elit id nulla cursus ultrices.</div>
+
+            <div className='about-right'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et orci consequat, fermentum purus ut, efficitur dolor. Duis facilisis mollis mauris sed commodo. Donec consequat pulvinar magna, id suscipit arcu tincidunt eu. Nullam condimentum libero enim, ut imperdiet nisl tincidunt vitae. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec sit amet elit porta, accumsan massa blandit, ultricies mauris. Vivamus risus dolor, porttitor a enim at, auctor mollis ante. Sed eu dictum erat. Fusce aliquet eget quam vitae ornare. Etiam orci enim, hendrerit eget varius id, commodo pulvinar leo. Sed eu pulvinar nulla. Etiam rutrum molestie lectus, id elementum est rutrum tempus. Pellentesque ac tristique odio. Phasellus eget elit id nulla cursus ultrices.</div>
+
+          </div>
        </div>
     );
 }
